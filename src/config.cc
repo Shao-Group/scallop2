f22436a06135d5cf1c0dc3ad54296d7cbdba32a7
Mingfu Shao <shaomingfu@gmail.com>
2021-06-30 19:57:19 +0000

    update

--- src/config.cc
+++ src/config.cc
@@ -72,7 +72,7 @@ double max_decompose_error_ratio[7] = {0.33, 0.05, 0.0, 0.25, 0.30, 0.0, 1.1};
 // for selecting paths
 double min_transcript_coverage = 1.5; //1.01->0.5 single cell;
 double min_transcript_coverage_ratio = 0.005;
-double min_single_exon_coverage = 20;
+double min_single_exon_coverage = 25;
 double min_transcript_numreads = 10; // 20 -> 10 single cell
 int min_transcript_length_base = 150;
 int min_transcript_length_increase = 50;
