7adb43addb0883314f1cbd3b2795f8a79e7e2413
Mingfu Shao <shaomingfu@gmail.com>
2021-08-18 00:25:47 -0400

    update

--- src/assembler.cc
+++ src/assembler.cc
@@ -165,8 +165,8 @@ int assembler::process(int n)
 		assemble(bd.gr, bd.hs, ts1, ts2);
 		*/
 
-		vector<transcript> gv1 = ts1.get_transcripts(4, 3);
-		vector<transcript> gv2 = ts2.get_transcripts(4, 3);
+		vector<transcript> gv1 = ts1.get_transcripts(4, 2);
+		vector<transcript> gv2 = ts2.get_transcripts(4, 2);
 
 		filter ft1(gv1);
 		ft1.filter_length_coverage();
