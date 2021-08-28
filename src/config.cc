8a95b8b813b3de076d2c7044400829519bbfe882
Mingfu Shao <shaomingfu@gmail.com>
2021-08-28 04:01:03 +0000

    update; tune back to 1.5

--- src/config.cc
+++ src/config.cc
@@ -70,7 +70,7 @@ double max_intron_contamination_coverage = 2.0;
 double max_decompose_error_ratio[7] = {0.33, 0.05, 0.0, 0.25, 0.30, 0.0, 1.1};
 
 // for selecting paths
-double min_transcript_coverage = 1.2; //1.01->0.5 single cell;
+double min_transcript_coverage = 1.5; //1.01->0.5 single cell;
 double min_transcript_coverage_ratio = 0.005;
 double min_single_exon_coverage = 30;
 double min_transcript_numreads = 10; // 20 -> 10 single cell
