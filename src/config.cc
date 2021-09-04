72a2405e771a039654e0b472a19f54ecb7a783c3
Mingfu Shao <shaomingfu@gmail.com>
2021-09-04 04:58:53 +0000

    tune gap

--- src/config.cc
+++ src/config.cc
@@ -58,7 +58,7 @@ bool use_overlap_scoring = false;
 int32_t max_clustering_flank = 30;
 int32_t flank_tiny_length = 10;
 double flank_tiny_ratio = 9.9;
-int32_t max_filling_gap = 150;
+int32_t max_filling_gap = 250;
 
 // for identifying subgraphs
 int32_t min_subregion_gap = 3;
