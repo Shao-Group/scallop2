a464252c7846ae0afe5ee43ee77f48bca06dc53a
Mingfu Shao <shaomingfu@gmail.com>
2023-06-15 20:11:24 +0000

    debug

--- src/bundle.cc
+++ src/bundle.cc
@@ -22,7 +22,19 @@ See LICENSE for licensing.
 bundle::bundle(bundle_base &b, reference &r)
 	: bb(b), br(b), ref(r)
 {
+	compute_strand();
+	if(bb.strand != '.')
+	{
+		printf("input strand = %c\n",bb.strand);
+	}
 	br.ref_trsts = ref.get_overlapped_transcripts(bb.chrm, bb.strand, bb.lpos, bb.rpos);
+
+	printf("range = %d-%d, strand = %c, #ref-trsts = %lu\n", bb.lpos, bb.rpos, bb.strand, br.ref_trsts.size());
+
+	if(br.ref_trsts.size() > 0)
+	{
+		printf("ref_trsts size: %lu\n",br.ref_trsts.size());
+	}
 	br.build();
 	prepare();
 }
@@ -32,7 +44,7 @@ bundle::~bundle()
 
 int bundle::prepare()
 {
-	compute_strand();
+	//compute_strand();
 	build_intervals();
 	build_junctions();
 
