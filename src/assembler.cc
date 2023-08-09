ca4c92694317a4df684c5d155d70302fd5cdbf5c
Tasfia Zahin <tkz5115@psu.edu>
2023-08-08 22:17:00 -0400

    multiexon no threshold, single exon 50

--- src/assembler.cc
+++ src/assembler.cc
@@ -312,9 +312,9 @@ int assembler::remove_duplicate_circ_trsts()
 			//printf("%s & %s\n",circ.circRNA_id.c_str(),old_circ.circRNA_id.c_str());
 			//printf("start diff %d. end diff %d, hash1 %s, hash2 %s\n",abs(circ.start-old_circ.start),abs(circ.end-old_circ.end),intron_chain_hash.c_str(),old_intron_chain_hash.c_str());
 
-			int end_diff = 100;
-			//abs(circ.start-old_circ.start) < 100 && abs(circ.end-old_circ.end) < 100 
-			if((intron_chain_hash != "" && intron_chain_hash == old_intron_chain_hash && abs(circ.start-old_circ.start) < end_diff && abs(circ.end-old_circ.end) < end_diff) || (intron_chain_hash == "" && intron_chain_hash == old_intron_chain_hash && abs(circ.start-old_circ.start) < end_diff && abs(circ.end-old_circ.end) < end_diff))
+			int end_diff = 50;
+			//&& abs(circ.start-old_circ.start) < end_diff && abs(circ.end-old_circ.end) < end_diff 
+			if((intron_chain_hash != "" && intron_chain_hash == old_intron_chain_hash) || (intron_chain_hash == "" && intron_chain_hash == old_intron_chain_hash && abs(circ.start-old_circ.start) < end_diff && abs(circ.end-old_circ.end) < end_diff))
 			{
 				if(circ.coverage > old_circ.coverage)
 				{
