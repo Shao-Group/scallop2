25ab462f27a9097680ff99657b8887963da77935
Tasfia Zahin <tkz5115@psu.edu>
2023-09-05 11:50:29 -0400

    when merging same intron chain but diff ends, enter non RO reads first then check RO reads to merge

--- src/assembler.cc
+++ src/assembler.cc
@@ -312,7 +312,7 @@ int assembler::remove_duplicate_circ_trsts()
 		//printf("key = %s, count = %d\n",itn->first.c_str(),itn->second.second);
 	}
 
-	//merge circRNAs that have different end boundaries but same intron chain into that with higher coverage (chimeric ones)
+	//merge circRNAs that have different end boundaries but same intron chain into that with higher coverage (chimeric)
 	for(itn = circ_trst_map.begin(); itn != circ_trst_map.end(); itn++)
 	{
 		//printf("start of check\n");
@@ -377,7 +377,7 @@ int assembler::remove_duplicate_circ_trsts()
 		}
 	}
 
-	//merge circRNAs that have different end boundaries but same intron chain into that with higher coverage (reverse overlap ones)
+	//merge circRNAs that have different end boundaries but same intron chain into that with higher coverage (reverse overlap)
 	for(itn = circ_trst_map.begin(); itn != circ_trst_map.end(); itn++)
 	{
 		//printf("start of check\n");
