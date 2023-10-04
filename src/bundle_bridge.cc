38ce0d7a9be1e3e7e3479c48a1c43dbfa3bda4d7
Mingfu Shao <shaomingfu@gmail.com>
2023-10-04 04:49:12 +0000

    more print

--- src/bundle_bridge.cc
+++ src/bundle_bridge.cc
@@ -1586,8 +1586,22 @@ int bundle_bridge::extend_junctions()
 
 		// triggle to split intervals
 		printf("EXTEND junction: %d-%d\n", low32(it->first), high32(it->first));
+
+		if(lpos == 119019188) print_split_interval_map(bb.mmap);
+
 		bb.mmap += make_pair(ROI(high32(it->first) - 2, high32(it->first)), 99);
+
+		printf("-----\n");
+
+		if(lpos == 119019188) print_split_interval_map(bb.mmap);
+
 		bb.mmap += make_pair(ROI(low32(it->first), low32(it->first) + 2), 99);
+
+		printf("-----\n");
+
+		if(lpos == 119019188) print_split_interval_map(bb.mmap);
+
+		printf("=====\n");
 	}
 
 	//for(int i=0;i<junctions.size();i++)
