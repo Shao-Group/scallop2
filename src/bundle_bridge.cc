ccb09cb322cfbbc6483958c75f573c879fad0e6d
Mingfu Shao <shaomingfu@gmail.com>
2023-10-04 04:07:05 +0000

    continue debug

--- src/bundle_bridge.cc
+++ src/bundle_bridge.cc
@@ -1585,6 +1585,7 @@ int bundle_bridge::extend_junctions()
 		filtered_junctions.push_back(jc);
 
 		// triggle to split intervals
+		printf("EXTEND junction: %d-%d\n", low32(it->first), high32(it->first));
 		bb.mmap += make_pair(ROI(low32(it->first), low32(it->first) + 1), 0);
 		bb.mmap += make_pair(ROI(high32(it->first), high32(it->first) + 1), 0);
 	}
